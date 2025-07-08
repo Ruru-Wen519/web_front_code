dd1d901a64949764d631fa0916a2c0f962fa29a7
MSI\User <lisa051920@gmail.com>
2025-07-08 12:22:53 +0800

    【】 測試訊息

--- order_system/order_system.js
+++ order_system/order_system.js
@@ -13,14 +13,20 @@ angular.module('myApp', [])
             fetch('https://my-web-page-code.onrender.com/api/data', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify({account_input, password_input })
+                body: JSON.stringify({account_input, password_input})
             })
                 .then(response => response.json())
                 .then(data => {
-                    document.getElementById('result').textContent = JSON.stringify(data, null, 2);
+                    if(data.is_correct == '0'){
+                        alert('登入成功!');
+                    }else{
+                        alert('登入失敗!');
+                    }
+                    //document.getElementById('result').textContent = JSON.stringify(data, null, 2);
                 })
                 .catch(err => {
-                    document.getElementById('result').textContent = '發生錯誤：' + err;
+                    //document.getElementById('result').textContent = '發生錯誤：' + err;
+                    alert('發生錯誤：' + err);
                 });
             // $http.get('http://192.168.0.143:5000/api/data')
             //     .then(function (response) {
